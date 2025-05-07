18b7319310d71468c0572060f8209930715e75d2
Mark Christian Mallari <104248911+MarkMallari16@users.noreply.github.com>
2025-05-07 23:24:09 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,8 +6,9 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
+        //flatlaf setup
         FlatLightLaf.setup();
-
+        //invoking
         SwingUtilities.invokeLater(() -> {
             Login log = new Login();
             log.show();
